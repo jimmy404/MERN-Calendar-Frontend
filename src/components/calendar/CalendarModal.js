a4028c7190b67f6393778be5f4382685711ae982
Jimmy200 <bruno_pier@hotmail.com>
2021-03-04 19:50:09 -0300

    Adds settings for calendar date-picker

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
+import moment from 'moment';
+
+import DateTimePicker from 'react-datetime-picker';
 
 const customStyles = {
   content : {
@@ -14,9 +17,23 @@ const customStyles = {
 
 Modal.setAppElement('#root');
 
+const now = moment().minutes(0).seconds(0).add(1, 'hours');
+const nowPlus1hs = now.clone().add(1, 'hours');
+
 export const CalendarModal = () => {
 
+  const [ dateStart, setDateStart ] = useState( now.toDate() );
+  const [ dateEnd, setDateEnd ] = useState( nowPlus1hs.toDate() );
+
   const closeModal = () => {
+  };
+
+  const handleStartDateChange = (e) => {
+    setDateStart(e);
+  };
+
+  const handleEndDateChange = (e) => {
+    setDateEnd(e);
   };
 
   return (
@@ -29,51 +46,59 @@ export const CalendarModal = () => {
       className="modal"
       overlayClassName="modal-fondo"
     >
-<h1> Nuevo evento </h1>
-<hr />
-<form className="container">
-  <div className="form-group">
-      <label>Fecha y hora inicio</label>
-      <input className="form-control" placeholder="Fecha inicio" />
-  </div>
+      <h1> Nuevo evento </h1>
+      <hr />
+      <form className="container">
+        <div className="form-group">
+            <label>Fecha y hora inicio</label>
+            <DateTimePicker
+              onChange={ handleStartDateChange }
+              value={ dateStart }
+              className="form-control"
+            />
+        </div>
 
-  <div className="form-group">
-      <label>Fecha y hora fin</label>
-      <input className="form-control" placeholder="Fecha inicio" />
-  </div>
+        <div className="form-group">
+            <label>Fecha y hora fin</label>
+            <DateTimePicker
+              onChange={ handleEndDateChange }
+              value={ dateEnd }
+              minDate={ dateStart }
+              className="form-control"
+            />
+        </div>
+        <hr />
+        <div className="form-group">
+            <label>Titulo y notas</label>
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Título del evento"
+                name="title"
+                autoComplete="off"
+            />
+            <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
+        </div>
 
-  <hr />
-  <div className="form-group">
-      <label>Titulo y notas</label>
-      <input
-          type="text"
-          className="form-control"
-          placeholder="Título del evento"
-          name="title"
-          autoComplete="off"
-      />
-      <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
-  </div>
+        <div className="form-group">
+            <textarea
+                type="text"
+                className="form-control"
+                placeholder="Notas"
+                rows="5"
+                name="notes"
+            ></textarea>
+            <small id="emailHelp" className="form-text text-muted">Información adicional</small>
+        </div>
 
-  <div className="form-group">
-      <textarea
-          type="text"
-          className="form-control"
-          placeholder="Notas"
-          rows="5"
-          name="notes"
-      ></textarea>
-      <small id="emailHelp" className="form-text text-muted">Información adicional</small>
-  </div>
-
-  <button
-      type="submit"
-      className="btn btn-outline-primary btn-block"
-  >
-      <i className="far fa-save"></i>
-      <span> Guardar</span>
-  </button>
-</form>
+        <button
+            type="submit"
+            className="btn btn-outline-primary btn-block"
+        >
+            <i className="far fa-save"></i>
+            <span> Guardar</span>
+        </button>
+      </form>
     </Modal>
   );
 };

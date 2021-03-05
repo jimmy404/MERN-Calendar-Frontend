d31e36ed037408fe2a9e4f17d993735c031712f9
Jimmy200 <bruno_pier@hotmail.com>
2021-03-04 22:48:58 -0300

    Adds state for manage form & events

--- src/components/calendar/CalendarModal.js
+++ src/components/calendar/CalendarModal.js
@@ -25,15 +25,44 @@ export const CalendarModal = () => {
   const [ dateStart, setDateStart ] = useState( now.toDate() );
   const [ dateEnd, setDateEnd ] = useState( nowPlus1hs.toDate() );
 
+  const [formValues, setFormValues] = useState({
+    title: 'Evento',
+    notes: '',
+    start: now.toDate(),
+    end: nowPlus1hs.toDate(),
+  });
+
+  const { notes, title } = formValues;
+
+  const handleInputChange = ({target}) => {
+    setFormValues({
+      ...formValues,
+      [target.name]: target.value
+    });
+  };
+
   const closeModal = () => {
   };
 
   const handleStartDateChange = (e) => {
     setDateStart(e);
+    setFormValues({
+      ...formValues,
+      start: e,
+    });
   };
 
   const handleEndDateChange = (e) => {
     setDateEnd(e);
+    setFormValues({
+      ...formValues,
+      end: e,
+    });
+  };
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    console.log(formValues);
   };
 
   return (
@@ -48,7 +77,10 @@ export const CalendarModal = () => {
     >
       <h1> Nuevo evento </h1>
       <hr />
-      <form className="container">
+      <form
+        className="container"
+        onSubmit={ handleSubmitForm }
+      >
         <div className="form-group">
             <label>Fecha y hora inicio</label>
             <DateTimePicker
@@ -76,6 +108,8 @@ export const CalendarModal = () => {
                 placeholder="Título del evento"
                 name="title"
                 autoComplete="off"
+                value={ title }
+                onChange={ handleInputChange }
             />
             <small id="emailHelp" className="form-text text-muted">Una descripción corta</small>
         </div>
@@ -87,6 +121,8 @@ export const CalendarModal = () => {
                 placeholder="Notas"
                 rows="5"
                 name="notes"
+                value={ notes }
+                onChange={ handleInputChange }
             ></textarea>
             <small id="emailHelp" className="form-text text-muted">Información adicional</small>
         </div>

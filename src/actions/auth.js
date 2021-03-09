e9fe1ccbf878a8d5d4c41e971c2c40ea8121b2ce
Jimmy200 <bruno_pier@hotmail.com>
2021-03-09 15:34:05 -0300

    Adds dispatch action for startRegister

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2';
 import { fetchSinToken } from '../helpers/fetch';
 import { types } from '../types/types';
 
@@ -14,8 +15,29 @@ export const startLogin = (email, password) => {
         uid: body.uid,
         name: body.name
       }));
+    } else {
+      Swal.fire('Error', body.msg, 'error')
     }
 
+  };
+};
+
+export const startRegister = (email, password, name) => {
+  return async (dispatch) => {
+    const resp = await fetchSinToken('auth/new', {email, password, name}, 'POST');
+    const body = await resp.json();
+
+    if( body.ok ) {
+      localStorage.setItem('token', body.token);
+      localStorage.setItem('token-init-date', new Date().getTime());
+
+      dispatch(login({
+        uid: body.uid,
+        name: body.name
+      }));
+    } else {
+      Swal.fire('Error', body.msg, 'error')
+    }
   };
 };
 

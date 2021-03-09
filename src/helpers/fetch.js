4862e987f6b5eaa71365f6f4ee7e26fc2bf7e308
Jimmy200 <bruno_pier@hotmail.com>
2021-03-09 16:16:14 -0300

    Adds fetchConToken function

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,6 +1,6 @@
 
 const baseUrl = process.env.REACT_APP_API_URL;
-console.log(baseUrl)
+
 const fetchSinToken = ( endpoint, data, method = 'GET' ) => {
   const url = `${baseUrl}/${endpoint}`;
 
@@ -17,6 +17,30 @@ const fetchSinToken = ( endpoint, data, method = 'GET' ) => {
   }
 };
 
+const fetchConToken = ( endpoint, data, method = 'GET' ) => {
+  const url = `${baseUrl}/${endpoint}`;
+  const token = localStorage.getItem('token') || '';
+
+  if (method === 'GET') {
+    return fetch( url, {
+      method,
+      headers: {
+        'x-token': token
+      }
+    } );
+  } else {
+    return fetch(url, {
+      method,
+      headers: {
+        'Content-type': 'application/json',
+        'x-token': token
+      },
+      body: JSON.stringify(data)
+    });
+  }
+};
+
 export {
   fetchSinToken,
+  fetchConToken
 };

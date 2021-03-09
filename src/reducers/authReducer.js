224eb4d33e7d26c80056b6845e8c909c9b1505e3
Jimmy200 <bruno_pier@hotmail.com>
2021-03-09 16:16:48 -0300

    Adds case for authCheckingFinish

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -9,8 +9,13 @@ export const authReducer = (state = initialState, action) => {
     case types.authLogin:
       return {
         ...state,
+        ...action.payload,
         checking: false,
-        ...action.payload
+      }
+    case types.authCheckingFinish:
+      return {
+        ...state,
+        checking: false,
       }
     default:
       return state;

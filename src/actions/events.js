684f28d46d2906e043f3f5bfddc387cfced01270
Jimmy200 <bruno_pier@hotmail.com>
2021-03-10 13:42:44 -0300

    Adds eventStartDelete

--- src/actions/events.js
+++ src/actions/events.js
@@ -47,7 +47,7 @@ export const eventStartUpdate = (event) => {
       const resp = await fetchConToken(`events/${event.id}`, event, 'PUT');
       const body = await resp.json();
       if (body.ok) {
-        dispatch(eventUpdated(event))
+        dispatch(eventUpdated(event));
       } else {
         Swal.fire('Error', body.msg, 'error');
       }
@@ -62,7 +62,24 @@ const eventUpdated = (event) => ({
   payload: event
 });
 
-export const eventDeleted = () => ({
+export const eventStartDelete = () => {
+  return async (dispatch, getState) => {
+    const { id } = getState().calendar.activeEvent;
+    try {
+      const resp = await fetchConToken(`events/${id}`, {}, 'DELETE');
+      const body = await resp.json();
+      if (body.ok) {
+        dispatch(eventDeleted());
+      } else {
+        Swal.fire('Error', body.msg, 'error');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+const eventDeleted = () => ({
   type: types.eventDeleted,
 });
 
